986a98504487f2ec838ea34bd09307d88858cdb8
Jesus <jrmougan@gmail.com>
2024-05-18 22:51:02 +0000

    Elimina cabeceras (prueba)

--- scrapyrealestate/scrapyrealestate/spiders/fotocasa_spider.py
+++ scrapyrealestate/scrapyrealestate/spiders/fotocasa_spider.py
@@ -1,3 +1,4 @@
+import random
 import scrapy, os, logging, time, json
 from scrapyrealestate.proxies import *
 from scrapy.spiders import CrawlSpider, Rule
@@ -7,51 +8,50 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 #from items import ScrapyrealestateItem # ERROR (es confundeix amb items al 192.168.1.100)
 from scrapyrealestate.items import ScrapyrealestateItem
+from scrapy_playwright.page import PageMethod
 
-class FotocasaSpider(CrawlSpider):
+
+class FotocasaSpider(scrapy.Spider):
     name = "fotocasa"
     allowed_domains = ["fotocasa"]
 
-    def start_requests(self):
-        # start_urls = [url + '?ordenado-por=fecha-publicacion-desc' for url in self.start_urls]
-        yield scrapy.Request(f'{self.start_urls}')
+    
 
-    custom_settings = {
-        # 'DOWNLOAD_DELAY': 2,
-        # 'ITEM_PIPELINES': {
-        #    'freedom.pipelines.IndexPipeline': 300
-        # },
-        "FEEDS": {"data/fotocasa.json": {"format": "json"}},
-        'DEFAULT_REQUEST_HEADERS': {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'es-ES,es;q=0.9,ca;q=0.8,en;q=0.7',
-            'cache-control': 'max-age=0',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'sec-gpc': '1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-        }
-    }
+
+    def start_requests(self):
+    
+        #start_urls = [url + '?ordenado-por=fecha-publicacion-desc' for url in self.start_urls]
+        yield scrapy.Request(f'{self.start_urls}',meta={
+                    'playwright': True,
+                    'playwright_context': 'fotocasa',                   
+                    "playwright_page_methods": [
+                    # click on the first product card
+                    PageMethod("wait_for_selector", ".re-CardPackAdvance"),
+                ],
+                })
+        
 
     def parse(self, response):
         # Import items
         items = ScrapyrealestateItem()
+        
+        test = response.css('article.re-CardPackAdvance')
+        logging.warning(f'test: {test}')
+
 
         # Necessary in order to create the whole link towards the website
         default_url = 'https://fotocasa.es'
         # Passem la resposta a text amb BeautifulSoup
         soup = BeautifulSoup(response.text, 'lxml')
+        logging.warning(f'soup: {soup}')
 
         # Agafem els div de tots els habitatges de la pàgina
         # <div class="item-info-container"><a aria-level="2" class="item-link" href="/inmueble/95416252/?xtmc=2_1_08030&amp;xtcr=0" role="heading" title="Piso en calle de Concepción Arenal, Sant Andreu, Barcelona">Piso en calle de Concepción Arenal, Sant Andreu, Barcelona</a><div class="price-row"><span class="item-price h2-simulated">800<span class="txt-big">€/mes</span></span></div><span class="item-detail">3 <small>hab.</small></span><span class="item-detail">60 <small>m²</small></span><span class="item-detail">Planta 3ª <small>exterior con ascensor</small></span><span class="item-detail"><small class="txt-highlight-red">5 horas</small></span><div class="item-description description"><p class="ellipsis">Piso con salón luminoso y habitación doble soleada. Dispone de tres habitaciones: una doble y dos individuales. Baño y cocina en perfecto...</p></div><div class="item-toolbar"><span class="icon-phone item-not-clickable-phone">935 437 953</span><a class="icon-phone phone-btn item-clickable-phone" href="tel:+34 935437953" target="_blank"><span>Llamar</span></a><button class="icon-chat email-btn action-email fake-anchor"><span>Contactar</span></button><button class="favorite-btn action-fav fake-anchor" data-role="add" data-text-add="Guardar" data-text-remove="Favorito" title="Guardar"><i class="icon-heart" role="image"></i><span>Guardar</span></button><button class="icon-delete trash-btn action-discard fake-anchor" data-role="add" data-text-remove="Descartar" rel="nofollow" title="Descartar"></button></div></div>
         # flats = response.css('div.item-info-container')
         # div --> class="item-info-container"
         # flats = soup.find_all("div", {"class": "re-Searchresult-itemRow"})
-        flats = soup.find_all("div", {"class": "re-CardPackMinimal-info"})  # Canvi de div - 10/11/2021
+        flats = soup.find_all("article", {"class": "re-CardPackAdvance"})  # Canvi de div - 10/11/2021
+        logging.warning(f'flats: {len(flats)}')
 
         # div --> class="pagination" --> ul --> li --> class="next"
         try:
@@ -65,8 +65,9 @@ class FotocasaSpider(CrawlSpider):
             # href = flats[nflat].find("a", {"class": "item-link"})['href']
             try:
                 # href = flats[nflat].find("a", {"class": "re-CardPackMinimal-slider"}, href=True)['href']
-                href = flats[nflat].find("a", {"class": "re-CardPackMinimal-info-container"}, href=True)[
+                href = flats[nflat].find("a", {"class": "re-CardPackAdvance-info"}, href=True)[
                     'href']  # Canvi de div - 10/11/2021
+                logging.warning(f'href: {href}')
 
             except:
                 break;  # Si no troba res sortim del bucle
