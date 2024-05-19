f85b8cf0eba3699bc104182705ab494f9c0f0590
Jesús Romeo <jrmougan@gmail.com>
2024-05-19 11:47:55 +0000

    Arregla Fotocasa con fix de m2

--- scrapyrealestate/scrapyrealestate/spiders/fotocasa_spider.py
+++ scrapyrealestate/scrapyrealestate/spiders/fotocasa_spider.py
@@ -1,4 +1,3 @@
-import random
 import scrapy, os, logging, time, json
 from scrapyrealestate.proxies import *
 from scrapy.spiders import CrawlSpider, Rule
@@ -8,42 +7,35 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 #from items import ScrapyrealestateItem # ERROR (es confundeix amb items al 192.168.1.100)
 from scrapyrealestate.items import ScrapyrealestateItem
-from scrapy_playwright.page import PageMethod
 
 
 class FotocasaSpider(scrapy.Spider):
     name = "fotocasa"
-    allowed_domains = ["fotocasa"]
+    allowed_domains = ["fotocasa.es"]
 
     
-
-
     def start_requests(self):
-    
         #start_urls = [url + '?ordenado-por=fecha-publicacion-desc' for url in self.start_urls]
-        yield scrapy.Request(f'{self.start_urls}',meta={
+        logging.info(self.start_urls)
+
+        yield scrapy.Request(self.start_urls,meta={
                     'playwright': True,
-                    'playwright_context': 'fotocasa',                   
-                    "playwright_page_methods": [
-                    # click on the first product card
-                    PageMethod("wait_for_selector", ".re-CardPackAdvance"),
-                ],
-                })
+                    'playwright_include_page': True,
+                    'playwright_viewport_size': '1920,1080',
+                    
+                } )
         
 
     def parse(self, response):
         # Import items
         items = ScrapyrealestateItem()
         
-        test = response.css('article.re-CardPackAdvance')
-        logging.warning(f'test: {test}')
 
 
         # Necessary in order to create the whole link towards the website
         default_url = 'https://fotocasa.es'
         # Passem la resposta a text amb BeautifulSoup
         soup = BeautifulSoup(response.text, 'lxml')
-        logging.warning(f'soup: {soup}')
 
         # Agafem els div de tots els habitatges de la pàgina
         # <div class="item-info-container"><a aria-level="2" class="item-link" href="/inmueble/95416252/?xtmc=2_1_08030&amp;xtcr=0" role="heading" title="Piso en calle de Concepción Arenal, Sant Andreu, Barcelona">Piso en calle de Concepción Arenal, Sant Andreu, Barcelona</a><div class="price-row"><span class="item-price h2-simulated">800<span class="txt-big">€/mes</span></span></div><span class="item-detail">3 <small>hab.</small></span><span class="item-detail">60 <small>m²</small></span><span class="item-detail">Planta 3ª <small>exterior con ascensor</small></span><span class="item-detail"><small class="txt-highlight-red">5 horas</small></span><div class="item-description description"><p class="ellipsis">Piso con salón luminoso y habitación doble soleada. Dispone de tres habitaciones: una doble y dos individuales. Baño y cocina en perfecto...</p></div><div class="item-toolbar"><span class="icon-phone item-not-clickable-phone">935 437 953</span><a class="icon-phone phone-btn item-clickable-phone" href="tel:+34 935437953" target="_blank"><span>Llamar</span></a><button class="icon-chat email-btn action-email fake-anchor"><span>Contactar</span></button><button class="favorite-btn action-fav fake-anchor" data-role="add" data-text-add="Guardar" data-text-remove="Favorito" title="Guardar"><i class="icon-heart" role="image"></i><span>Guardar</span></button><button class="icon-delete trash-btn action-discard fake-anchor" data-role="add" data-text-remove="Descartar" rel="nofollow" title="Descartar"></button></div></div>
@@ -65,7 +57,7 @@ class FotocasaSpider(scrapy.Spider):
             # href = flats[nflat].find("a", {"class": "item-link"})['href']
             try:
                 # href = flats[nflat].find("a", {"class": "re-CardPackMinimal-slider"}, href=True)['href']
-                href = flats[nflat].find("a", {"class": "re-CardPackAdvance-info"}, href=True)[
+                href = flats[nflat].find("a", {"class": "re-CardPackAdvance-info-container"}, href=True)[
                     'href']  # Canvi de div - 10/11/2021
                 logging.warning(f'href: {href}')
 
@@ -95,7 +87,7 @@ class FotocasaSpider(scrapy.Spider):
                 rooms = ''
 
             try:
-                m2 = flats[nflat].find_all("span", {"class": "re-CardFeatures-feature"})[2].text.strip()
+                m2 = flats[nflat].find("span", {"class": "re-CardFeaturesWithIcons-feature-icon--surface"}).text.strip()
             except:
                 m2 = ''
 
